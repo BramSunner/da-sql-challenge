fbb77ad13f96c857482c830d3e0e6a6293ca7cba
BramSunner <ethanmbasura@gmail.com>
2024-06-20 18:13:30 -0400

    final update, minus readme, will do that later

---
+++ schema/table_creation.sql
@@ -0,0 +1,84 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "employees" (
+    "emp_no" INTEGER   NOT NULL,
+    "emp_title" VARCHAR(5)   NOT NULL,
+    "birth_date" DATE   NOT NULL,
+    "first_name" VARCHAR(50)   NOT NULL,
+    "last_name" VARCHAR(50)   NOT NULL,
+    "sex" VARCHAR(1)   NOT NULL,
+    "hire_date" DATE   NOT NULL,
+    "last_updated" TIMESTAMP   DEFAULT NOW(),
+    CONSTRAINT "pk_employees" PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+CREATE TABLE "department" (
+    "dept_no" VARCHAR(5)   NOT NULL,
+    "dept_name" VARCHAR(50)   NOT NULL,
+    "last_updated" TIMESTAMP   DEFAULT NOW(),
+    CONSTRAINT "pk_department" PRIMARY KEY (
+        "dept_no"
+     )
+);
+
+CREATE TABLE "departmentemployee" (
+    "id" SERIAL   NOT NULL,
+    "emp_no" INTEGER   NOT NULL,
+    "dept_no" VARCHAR(5)   NOT NULL,
+    "last_updated" TIMESTAMP	DEFAULT NOW(),
+    CONSTRAINT "pk_departmentemployee" PRIMARY KEY (
+        "id"
+     )
+);
+
+CREATE TABLE "departmentmanager" (
+    "id" SERIAL   NOT NULL,
+    "dept_no" VARCHAR(5)   NOT NULL,
+    "emp_no" INTEGER   NOT NULL,
+    "last_updated" TIMESTAMP   DEFAULT NOW(),
+    CONSTRAINT "pk_departmentmanager" PRIMARY KEY (
+        "id"
+     )
+);
+
+CREATE TABLE "salaries" (
+    "id" SERIAL   NOT NULL,
+    "emp_no" INTEGER   NOT NULL,
+    "salary" INTEGER   NOT NULL,
+    "last_updated" TIMESTAMP   DEFAULT NOW(),
+    CONSTRAINT "pk_salaries" PRIMARY KEY (
+        "id"
+     )
+);
+
+CREATE TABLE "titles" (
+    "title_id" VARCHAR(5)   NOT NULL,
+    "title" VARCHAR(50)   NOT NULL,
+    "last_updated" TIMESTAMP   DEFAULT NOW(),
+    CONSTRAINT "pk_titles" PRIMARY KEY (
+        "title_id"
+     )
+);
+
+ALTER TABLE "employees" ADD CONSTRAINT "fk_employees_emp_title" FOREIGN KEY("emp_title")
+REFERENCES "titles" ("title_id");
+
+ALTER TABLE "departmentemployee" ADD CONSTRAINT "fk_departmentemployee_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "departmentemployee" ADD CONSTRAINT "fk_departmentemployee_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "department" ("dept_no");
+
+ALTER TABLE "departmentmanager" ADD CONSTRAINT "fk_departmentmanager_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "department" ("dept_no");
+
+ALTER TABLE "departmentmanager" ADD CONSTRAINT "fk_departmentmanager_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
---
+++ schema/table_deletion.sql
@@ -0,0 +1,8 @@
+-- Drop the tables.
+
+DROP TABLE employees CASCADE;
+DROP TABLE salaries CASCADE;
+DROP TABLE titles CASCADE;
+DROP TABLE departmentemployee CASCADE;
+DROP TABLE departmentmanager CASCADE;
+DROP TABLE department CASCADE;
